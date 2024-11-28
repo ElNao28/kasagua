2c20141648666ed1bafb22afc6b935ec92605196
ElNao <naopubg28@gmail.com>
2024-11-28 10:47:07 -0600

    se añadio la animacion de entrada al contenedor de nosotros

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+
+export class HomeComponent implements AfterViewInit{
+
+  @ViewChild('nosotros') container!: ElementRef;
+  public visible:boolean = false;
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries)=>{
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          console.log('Element is visible')
+          this.visible = true;
+        }
+      })
+    })
+    observer.observe(this.container.nativeElement)
+  }
 
 }
