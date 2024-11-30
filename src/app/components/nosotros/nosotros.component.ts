ee2c08d824757fe8dcd93dfd48b6786e6b675e34
ElNao <naopubg28@gmail.com>
2024-11-30 00:21:04 -0600

    se arreglo la interseccion para las animaciones de nosotros

--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -12,13 +12,18 @@ export class NosotrosComponent implements AfterViewInit {
   @ViewChild('nosotros') container!: ElementRef;
   public visible: boolean = false;
   ngAfterViewInit(): void {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          this.visible = true;
-        }
+    const observer = new IntersectionObserver(entries =>
+      this.viewContainer(entries),{
+        threshold: 0.5,
+        rootMargin: '0px 0px 0px 0px'
       });
-    });
     observer.observe(this.container.nativeElement);
+  }
+
+  private viewContainer(entries: IntersectionObserverEntry[]) {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      this.visible = true;
+    }
   }
 }
