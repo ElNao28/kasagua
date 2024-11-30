c6ac624e802dd704feb26c7cacb6bee303e39647
ElNao <naopubg28@gmail.com>
2024-11-30 16:59:34 -0600

    limpieza del codigo

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,5 +1,5 @@
-import { NgClass, NgIf } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
