48ea617df1502e0ce8e2fdd704bd4cc993cfe634
ElNao <naopubg28@gmail.com>
2024-11-30 16:57:00 -0600

    se termino el componente de galeria

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { NgClass, NgIf } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [RouterLink, NgClass],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  @ViewChild('view') viewContainer!: ElementRef;
+  public isExpand: boolean = false;
+  public expandImg(url:string) {
+    this.isExpand = !this.isExpand;
+    if(!this.isExpand){
+      this.viewContainer.nativeElement.removeChild(this.viewContainer.nativeElement.querySelector('img'));
+      return;
+    }
+    const img = document.createElement('img');
+    img.src = url;
+    img.alt = 'img view';
+    img.className = 'fixed top-1 z-20 rounded-md h-full';
+    this.viewContainer.nativeElement.appendChild(img);
 
+  }
 }
