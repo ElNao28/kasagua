5975e268c01bb5278bd39ea430effb326a8cbbba
ElNao <naopubg28@gmail.com>
2024-11-29 23:18:06 -0600

    estilos a la barra de navegacion en blog

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgClass],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+  @Input()
+  type:boolean = false;
 }
