b1bed8d2a154e6edc567081bca284d327916c3a0
ElNao <naopubg28@gmail.com>
2024-11-29 21:25:44 -0600

    se añadio animacion a las imagenes de servivios

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-servicios',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 })
-export class ServiciosComponent {
+export class ServiciosComponent implements AfterViewInit{
+  @ViewChild('items1') items1!:ElementRef;
 
+  public visibleItem1:boolean = false;
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(entries => {
+      const item = entries[0];
+      if(item.isIntersecting){
+        this.visibleItem1 = true;
+      }
+    });
+    observer.observe(this.items1.nativeElement);
+  }
 }
