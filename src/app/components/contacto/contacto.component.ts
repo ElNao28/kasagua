f3409e22f079681b334afc7905a05e27d90f830a
ElNao <naopubg28@gmail.com>
2024-12-02 20:24:41 -0600

    se creo el formulario de contacto

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ConexionService } from '../../services/conexion.service';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrl: './contacto.component.css',
 })
 export class ContactoComponent {
+  constructor(private conexionService: ConexionService) {}
+  private fb: FormBuilder = inject(FormBuilder);
 
+  public formContact: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required]],
+    apellido: ['', [Validators.required]],
+    telefono: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    fecha: ['', [Validators.required]],
+    cantidad: ['', [Validators.required]],
+  });
+
+  public sendMessage(){
+    console.log(this.formContact.value)
+  }
 }
